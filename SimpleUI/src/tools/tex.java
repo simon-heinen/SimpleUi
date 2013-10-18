9cafda4bdb78f1790dfd1789dcb383dd21e29414
Bitstars <simon@bitstars.com>
2013-10-18 11:24:49 +0200

    small improvements

--- SimpleUI/src/tools/tex.java
+++ SimpleUI/src/tools/tex.java
@@ -1,5 +1,6 @@
 package tools;
 
+import v2.simpleUi.SimpleUiApplication;
 import android.content.Context;
 
 public class tex {
@@ -11,10 +12,24 @@ public class tex {
 	}
 
 	public static String t(int resId) {
+		if (c == null) {
+			c = SimpleUiApplication.getContext();
+		}
 		return "" + c.getText(resId);
 	}
 
+	/**
+	 * @param resId
+	 * @param s
+	 *            use %1$s %2$s ... in the string to replace the passed dynamic
+	 *            values. For example a string coud be
+	 *            "Eat %1$s apples and %2$s bananas"
+	 * @return
+	 */
 	public static String t(int resId, String... s) {
+		if (c == null) {
+			c = SimpleUiApplication.getContext();
+		}
 		return "" + c.getString(resId, s);
 	}
 }
