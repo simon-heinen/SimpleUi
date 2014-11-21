71df3bd625770e89742ed1d7657163c5f46b67d2
simon-heinen <simon@bitstars.com>
2014-11-21 14:52:47 +0100

    added check if the device is a google glass

--- SimpleUI/srcJava/util/SystemUtil.java
+++ SimpleUI/srcJava/util/SystemUtil.java
@@ -3,6 +3,27 @@ package util;
 public class SystemUtil {
 
 	private static Boolean isAndroid;
+	private static Boolean isGlass;
+
+	public static boolean isGlass() {
+		if (isAndroid()) {
+			if (isGlass == null) {
+				try {
+					Class<?> Build = Class.forName("android.os.Build");
+					String Build_MANUFACTURER = (String) Build
+							.getDeclaredField("MANUFACTURER").get(String.class);
+					String Build_MODEL = (String) Build.getDeclaredField(
+							"MODEL").get(String.class);
+					isGlass = "Google".equalsIgnoreCase(Build_MANUFACTURER)
+							&& Build_MODEL.startsWith("Glass");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return isGlass;
+		}
+		return false;
+	}
 
 	public static boolean isAndroid() {
 		if (isAndroid == null) {
